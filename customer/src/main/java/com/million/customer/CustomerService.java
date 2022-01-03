c9591d0ddbc087e87ba09338412b9bf8c84ccdd2
John Million <john.million@rtslabs.com>
2022-01-03 08:08:40 -0500

    Check if fraud in customer controller

--- customer/src/main/java/com/million/customer/CustomerService.java
+++ customer/src/main/java/com/million/customer/CustomerService.java
@@ -2,12 +2,14 @@ package com.million.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @AllArgsConstructor
 @Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -18,6 +20,17 @@ public class CustomerService {
 
         // todo: check if email valid
         // todo: check if email not taken
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        // todo: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http:localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+        // todo: send notification
     }
 }
---
+++ customer/src/main/java/com/million/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.million.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
